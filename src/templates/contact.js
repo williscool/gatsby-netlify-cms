959e6dee69f8c5c0c245ca9a54f553c9c13b3312
William Harris <wharris@upscalews.com>
2024-11-21 06:45:10 +0000

    style update

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -15,14 +15,14 @@ const ContactPage = () => (
     <div className="contactRedirect  container">
       <h1 className="contactRedirect-title">Redirecting...</h1>
       <img src="https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3d4eWZ2dThuaWdnM3ZqdHdseWx1cGpxaGRjbndsMmFmcW0yMjl1eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1kkxWqT5nvLXupUTwK/giphy.webp" alt="Computer Doggo"></img>
+      <a style={{marginTop: 40}} href='#'> Computer Doggo taking you there! </a>
       <p className="contactRedirect-description">{`
-(___________________________()6 \`-,
-(   ______________________   /''"\`
-//\\                      //\\
-"" ""                     "" ""
+    (___________________________()6 \`-,
+    (   ______________________   /''"\`
+    //\\                      //\\
+    "" ""                     "" ""
       `}</p>
 
-      <a href='#'> Computer Doggo taking you there! </a>
     </div>
   </Layout>
 );
